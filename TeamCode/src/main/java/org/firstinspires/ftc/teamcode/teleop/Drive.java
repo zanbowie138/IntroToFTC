51a50718d0770867e9bc735ce9efcb3b96e90579
Alex Bui <zanbowie138@gmail.com>
2024-02-29 17:51:08 -0600

    yes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Drive.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public class Drive extends OpMode {
+    private DcMotorEx leftFront, leftRear, rightRear, rightFront;
+
+    @Override
+    public void init() {
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        leftRear = hardwareMap.get(DcMotorEx.class, "leftRear");
+        rightRear = hardwareMap.get(DcMotorEx.class, "rightRear");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Gamepad 1 y", gamepad1.left_stick_y);
+    }
+}
